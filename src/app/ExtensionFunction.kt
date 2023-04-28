fbb671374c66125b35e5386df3092be195f5612d
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-04-28 16:09:21 +0700

    Belajar materi Extension Function

---
+++ src/app/ExtensionFunction.kt
@@ -0,0 +1,9 @@
+package app
+
+import data.Student
+import data.sayHello
+
+fun main() {
+    val syafiq = Student("Syafiq", 19)
+    syafiq.sayHello("Budi")
+}
---
+++ src/data/Student.kt
@@ -0,0 +1,17 @@
+package data
+
+/**
+ * Perlu diperhatikan, bahwa saat membuat extension function, kita tidak memodifikasi class aslinya
+ * Extension function hanyalah sebuah function bantuan yang artinya, kita tidak bisa mengakses data private atau protected dari class tersebut
+ */
+class Student(val name: String, val age: Int)
+
+/**
+ * Secara standar, extension function hanya bisa untuk data yang tidak null
+ * Jika kita ingin membuat extension function yang bisa digunakan untuk data yang bisa null, kita perlu menggunakan kata kunci ? (tanda tanya)
+ */
+fun Student?.sayHello(name: String) {
+    if(this != null) {
+        println("Hello $name, my name is ${this.name} and my age is ${this.age}")
+    }
+}
