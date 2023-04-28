8ea7ccbd194d9f42f2d5b06a8f9f186242ccec16
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-04-29 06:33:17 +0700

    Belajar materi Inline Class

---
+++ src/data/Token.kt
@@ -0,0 +1,10 @@
+package data
+
+/**
+ * Kadang kita hanya membuat class dengan satu properties
+ * Terlalu banyak class akan ada konsekuensinya, yaitu memakan konsumsi memory yang lebih banyak ketika program kita berjalan
+ * Kotlin memiliki kemampuan untuk mengubah class menjadi inline dengan menggunakan kata kunci inline
+ */
+inline class Token(val value: String) {
+    fun toUpper(): String = value.toUpperCase()
+}
