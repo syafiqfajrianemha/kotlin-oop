69ff2b54512e778a9ee6146fb708c7760f201a36
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-04-29 10:10:15 +0700

    Belajar materi Observable Properties

---
+++ src/app/ObservableProperties.kt
@@ -0,0 +1,11 @@
+package app
+
+import data.Account
+
+fun main() {
+    val account = Account()
+
+    account.descpripton = "Contoh"
+    account.descpripton = "Diubah"
+    account.descpripton = "Hello World"
+}
--- src/data/Account.kt
+++ src/data/Account.kt
@@ -1,13 +1,24 @@
 package data
 
+import kotlin.properties.Delegates
+
 /**
  * Selain delegating di class, Kotlin mendukung delegating di properties.
  * Salah satu implementasi delegating properties yang sudah disediakan oleh Kotlin adalah, lazy properties
  * Lazy adalah standar library yang telah disediakan agar properties baru diinisialisasi ketika properties itu diakses
  */
-class Account {
+class Account(desc: String = "") {
     val name: String by lazy {
         println("Name is called")
         "Syafiq"
+    }
+
+    /**
+     * Salah satu standar library yang disediakan oleh Kotlin untuk delegating di properties adalah observable properties
+     * Dengan observable properties, kita bisa tahu properties, value sebelum dan value setelah ketika diubah
+     */
+    var descpripton: String by Delegates.observable(desc) {
+        property, oldValue, newValue ->
+            println("${property.name} changed from $oldValue to $newValue")
     }
 }
